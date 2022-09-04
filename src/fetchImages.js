8a42d9f691263ae23040a04441cb077fe1a2c011
Piojank <98431706+Piojank@users.noreply.github.com>
2022-09-04 21:24:04 +0200

    fetchimages update

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -4,7 +4,7 @@ export async function fetchImages(searching, page) {
   try {
     const response = await axios({
       method: 'get',
-      url: ,
+      url: `https://pixabay.com/api/?key=29712287-5d117bcccbf45424d50c3eb4b&q=${searching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
     });
     return response.data;
   } catch (error) {
