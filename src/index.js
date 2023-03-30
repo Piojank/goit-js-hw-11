3412464b5def47dfe239c73f75df2c622b1b2658
piojank <p.jankowski931026@gmail.com>
2023-03-30 18:52:22 +0200

    add fetchImage js file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Notiflix from "notiflix";
+import debounce from "lodash.debounce";
+import simpleLightbox from "simplelightbox";
+
+const qs = (s) => document.querySelector(s);
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 
-export async function fetchImages(searching, page) {
+const fetchImages = async (searching, page) => {
+    const BASE_URL = "https://pixabay.com/api/";
     const API_KEY = "29712287-5d117bcccbf45424d50c3eb4b";
+    const FETCH_URL = `${BASE_URL}?key=${API_KEY}=${searching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
-    
+    try {
+        const response = axios({
+            method: 'get',
+            url: FETCH_URL,
+        });
+        return response.json();
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 
-}
+export { fetchImages };
+
