b0da82e37ddc17f79f9361707807a528fa4551f4
piojank <p.jankowski931026@gmail.com>
2023-03-31 20:02:20 +0200

    Add sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import Notiflix from "notiflix";
 import debounce from "lodash.debounce";
 import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const qs = (s) => document.querySelector(s);
+const gallery = qs('.gallery');
+
+// const renderGalleryPhotoBySearchTag = (photos) => {
+
+//     const markup = photos
+//         .map(
+//             (photo) => ``
+//         )
+//         .join("");
+//     gallery.innerHTML = markup;
+// };
