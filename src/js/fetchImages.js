571d58ec56a640633407d5e01442f30923c8d34f
piojank <p.jankowski931026@gmail.com>
2023-04-08 13:36:28 +0200

    fix per page

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchImages = async (searching, page) => {
     const BASE_URL = "https://pixabay.com/api/";
     const API_KEY = "29712287-5d117bcccbf45424d50c3eb4b";
-    const FETCH_URL = `${BASE_URL}?key=${API_KEY}&q=${searching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+    const FETCH_URL = `${BASE_URL}?key=${API_KEY}&q=${searching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=35`;
 
     try {
         const response = await axios.get(FETCH_URL);
